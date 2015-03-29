7afb4a15deb514017bbfaee72a21e8826c25fb61
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-03-29 09:47:08 -0700

    Initial Commit

---
+++ TargetSum/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TargetSum
+//
+//  Created by Rodrigo Carballo on 3/29/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
